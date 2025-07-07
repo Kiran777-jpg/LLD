fe63859642dd59b7322af777ff57e947fe5de623
Kiran777-jpg <68018374+Kiran777-jpg@users.noreply.github.com>
2025-07-07 22:23:48 +0530

    Added comment in ToMap class

--- src/main/java/org/example/streams/collectors/ToMap.java
+++ src/main/java/org/example/streams/collectors/ToMap.java
@@ -11,6 +11,7 @@ public class ToMap {
     public static void main(String[] args) {
         List<Student> students = StudentDataBase.getAllStudents();
 
+        // get all students and map using name
         Map<String, List<String>> studentsMap = students.stream()
                 .collect(Collectors.toMap(Student::getName, Student::getActivities));
 
