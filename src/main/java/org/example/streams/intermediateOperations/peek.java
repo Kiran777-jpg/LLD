576c7cf2a442e6ee5aced7d8e00eb34af0cb935c
Kiran777-jpg <suryakirananusuri@gmail.com>
2025-07-07 23:52:42 +0530

    Added peek() of stream

---
+++ src/main/java/org/example/streams/intermediateOperations/peek.java
@@ -0,0 +1,17 @@
+package org.example.streams.intermediateOperations;
+
+import org.example.streams.models.Student;
+import org.example.streams.models.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class peek {
+    public static void main(String[] args) {
+        List<Student> studentList = StudentDataBase.getAllStudents().stream()
+                .peek(student -> System.out.println(student.getName()))
+                .filter(student -> student.getGpa() > 3.6)
+                .peek(student -> System.out.println(student.getName()))
+                .collect(Collectors.toList());
+    }
+}
